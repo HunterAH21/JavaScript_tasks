dacd5ddf106f7ebfe26167884ae483eea30ef25e
HunterAH21 <116879213+HunterAH21@users.noreply.github.com>
2024-07-15 16:56:38 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,107 @@
+//1)Fizz-Buzz
+
+// let num = 100;
+
+// for(let i = 1;i<num;i++){
+//     if((i%3==0)&&(i%5==0)){
+//         console.log("FizzBuzz");
+//     }
+//     else if (i%3==0) {
+//         console.log("Fizz");
+//     } 
+//     else if(i%5==0){
+//         console.log("Buzz");
+//     }
+//     else{
+//         console.log(i);
+//     }
+    
+// }
+
+//2)palindrome
+
+// let text = prompt();
+// let text1 = "";
+// let ind = text.length-1;
+// for(let i=0;i<text.length;i++){
+//     text1 =text1.concat(text.charAt(ind));
+//     ind=ind-1;
+// }
+// if(text1==text){
+//     console.log("palendrome");
+// }
+// else{
+//     console.log("not a palendrome");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3)largest num
+
+// let arr = [1,2,3,4,5,6,7];
+// let max = 0;
+// for(let i=0 ; i<7 ; i++){
+//     if(arr[i]>max){
+//         max = arr[i]
+//     }
+// }
+// console.log(max);
+
+
+// 4)number of occurrence
+
+
+
+
+// 5) largest word
+
+
+
+
+
+
+// 6) factorial
+
+// let n = prompt();
+// let res = 1;
+// for(let i = 1;i<=n;i++){
+//     res = res*i;
+// }
+// console.log("the factorial of the given number is "+res);
+
+
+
+// 7) Celsius to Fahrenheit
+
+// let cel = prompt();
+
+// let f = ((9/5)*cel)+32;
+// console.log("the value of celsius to fahrenheit is "+f);
+
+
+// 8) checking of element
+
+// let arr1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20]
+
+// for(let i=1 ; i<=arr1.length;i++){
+//     if(i==arr1[i-1]){
+        
+//     }
+//     else{
+//         console.log("the missing element is "+i);
+//         break;
+//     }
+// }
+
+
